e1a6e40c312de7afb037b2f96ec11e36161a938b
Jeppe Vammen Kristensen <jeppe@jeppek.dk>
2014-11-26 19:21:16 +0100

    Refactored extensions and added "unittest"

---
+++ UnitTests/Utilities/ExtensionsTester.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using FileQuerier.Utilities;
+using NUnit.Framework;
+
+namespace UnitTests.Utilities
+{
+    public class Person
+    {
+        public Name Name { get; set; }
+        public string BirthTown { get; set; }
+        public DateTime Birthday { get; set; }
+        public List<string> Titles { get; set; }
+
+        public Name[] Aliases { get; set; }
+    }
+
+    public class Name
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+
+    [TestFixture]
+    public class ExtensionsTester
+    {
+        [Test]
+        public void Dump_CanDumpGeneralExample()
+        {
+            var generateTestData = TestData();
+            var oldWriter = Console.Out;
+            string result = string.Empty;
+
+            using (var writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+
+                generateTestData.Dump();
+                result = writer.ToString();
+            }
+
+            Console.SetOut(oldWriter);
+            Console.WriteLine(result);
+
+        }
+
+        private Person TestData()
+        {
+            return new Person
+            {
+                BirthTown = "Aalborg",
+                Aliases = new[]
+                {
+                    new Name()
+                    {
+                        FirstName = "El",
+                        LastName = "Boberino"
+                    },
+                    new Name()
+                    {
+                        FirstName = "Bill",
+                        LastName = "Mac Laughalot"
+                    }
+                },
+                Name = new Name()
+                {
+                    FirstName = "Jeppe",
+                    LastName = "Kristensen"
+                },
+                Birthday = new DateTime(1979, 10, 12),
+                Titles = new List<string>() { "Sir", "Master Jedi" }
+            };
+        }
+    }
+}
--- Utilities/EnumerableExtensions.cs
+++ Utilities/EnumerableExtensions.cs
@@ -18,37 +18,9 @@ namespace FileQuerier.Utilities
 
         public static T Dump<T>(this T source, int depth = 2)
         {
-            RecursiveDump(typeof(T), source,1, depth);
+            var typeDumper = new TypeDumper(depth);
+            typeDumper.VisitRecursive(source,0);
             return source;
-        }
-
-        private static void RecursiveDump(Type type, object source, int currentLevel, int depth)
-        {
-            if (currentLevel > depth)
-                return;
-            foreach (var property in type.GetProperties())
-            {
-                if (property.PropertyType.IsPrimitive || property.PropertyType == typeof(string))
-                {
-                    var value = property.GetValue(source, null);
-                    Console.WriteLine("\{property.Name}:\{value}");
-                }
-                else
-                {
-                    Console.WriteLine("{0}", property.Name);
-
-                    try
-                    {
-                        RecursiveDump(property.PropertyType, property.GetValue(source, null), currentLevel + 1,
-                         depth);
-                    }
-                    catch (Exception)
-                    {
-                        Console.WriteLine("Failed (x x)");
-                    }
-                }
-            }
-
         }
     }
 }
